70d2bf5286588c76ee3388c9b5f9a2d73e4f4510
Salar Ghotaslo <Salar_@live.co.uk>
2021-02-07 21:05:01 +0000

    changes score and changes bowl

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,10 +1,13 @@
 class Bowling
-attr_reader :total_score
+  attr_reader :total_score, :current_bowl
+
   def initialize
     @total_score = 0
-end
+    @current_bowl = 1
+  end
 
-def bowl(score)
-  @total_score += score
-end
+  def player_score(score)
+    @total_score += score
+    @current_bowl = 2
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,10 +1,17 @@
 require 'bowling'
 
-  describe Bowling do
-    let(:bowling) { Bowling.new }
-      it 'adds score to the total' do
-        bowling.bowl(3)
-        expect(bowling.total_score).to eq 3
-      end
-    end
+describe Bowling do
+  let(:bowling) { Bowling.new }
+  let(:score) { 7 }
+  it 'totals the score' do
+    bowling.player_score(3)
+    expect(bowling.total_score).to eq 3
   end
+  subject { bowling.player_score(score) }
+  it 'changes the total score by that amount' do
+    expect { subject }.to change { bowling.total_score }.by score
+  end
+  it 'goes to the next bowl' do
+    expect { subject }.to change { bowling.current_bowl }.to 2
+  end
+end
