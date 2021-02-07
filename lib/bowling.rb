1f80f32b262e3fd4e47c90fec719e3f442910b98
Salar Ghotaslo <Salar_@live.co.uk>
2021-02-07 19:59:09 +0000

    adds score to the total, starting at 0

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,2 +1,10 @@
 class Bowling
+attr_reader :total_score
+  def initialize
+    @total_score = 0
+end
+
+def bowl(score)
+  @total_score += score
+end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,5 +1,10 @@
 require 'bowling'
 
-describe Bowling do
-  
+  describe Bowling do
+    let(:bowling) { Bowling.new }
+      it 'adds score to the total' do
+        bowling.bowl(3)
+        expect(bowling.total_score).to eq 3
+      end
+    end
   end
